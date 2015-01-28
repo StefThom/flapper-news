9491ac177fb2456ae91ef2c7b22a74b1b3ab39ea
StefThom <jaapvdbelt@gmail.com>
2015-01-28 20:15:50 +0100

    InlineTemplates moved to external templates

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -11,7 +11,7 @@ angular.module('flapperNews', ['ui.router'])
             $stateProvider
                 .state('home', {
                     url: '/home',
-                    templateUrl: '/home.html',
+                    templateUrl: 'templates/home.html',
                     controller: 'MainCtrl',
                     resolve: {
                         postPromise: ['posts', function(posts) {
@@ -22,7 +22,7 @@ angular.module('flapperNews', ['ui.router'])
 
                 .state('posts', {
                     url: '/posts/{id}',
-                    templateUrl: '/posts.html',
+                    templateUrl: 'templates/posts.html',
                     controller: 'PostsCtrl',
                     resolve: {
                         post: ['$stateParams', 'posts', function($stateParams,posts) {
@@ -143,4 +143,4 @@ angular.module('flapperNews', ['ui.router'])
                 });
         };
         return o;
-    }])
+    }]);
